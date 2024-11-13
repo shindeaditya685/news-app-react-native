b21a81ceb1753b349874d6ee44d3216d2f4d44be
Aditya Shinde <shindeaditya685@gmail.com>
2024-11-13 18:00:36 +0530

    create search bar component

--- components/Header.tsx
+++ components/Header.tsx
@@ -32,6 +32,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    marginBottom: 20
   },
   userImage: {
     width: 50,
