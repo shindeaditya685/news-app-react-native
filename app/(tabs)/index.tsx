fda57ae02516a15dfd3036fbbb3a8c84e39971fa
Aditya Shinde <shindeaditya685@gmail.com>
2024-11-13 18:38:45 +0530

    fetch data from the news api

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,41 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Header from "@/components/Header";
 import SearchBar from "@/components/SearchBar";
+import axios from "axios";
+import { NewsDataType } from "@/types";
 
 type Props = {};
 
 const Page = (props: Props) => {
   const { top: safeTop } = useSafeAreaInsets();
+  const [breakingNews, setBreakingNews] = useState<NewsDataType[]>([]);
+
+  useEffect(() => {
+    getBreakingNews();
+  }, []);
+
+  const getBreakingNews = async () => {
+    try {
+      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&country=in&language=en&image=1&removeduplicate=1&size=5`;
+
+      const response = await axios.get(URL);
+      if (response && response.data) {
+        setBreakingNews(response.data.results);
+      }
+    } catch (error: any) {
+      console.log("Error occured while fetching the breaking news.", error);
+    }
+  };
+
   return (
     <View style={(styles.container, { paddingTop: safeTop })}>
       <Header />
-      <SearchBar/>
+      <SearchBar />
+      {breakingNews.map((item, index) => (
+        <Text key={index}>{item.title}</Text>
+      ))}
     </View>
   );
 };
