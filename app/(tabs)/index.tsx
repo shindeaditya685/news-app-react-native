46903b78c434c02632d8b5e3d3ad02cb7ddba186
Aditya Shinde <shindeaditya685@gmail.com>
2024-11-19 09:00:14 +0530

    News Details Screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,10 @@
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Header from "@/components/Header";
@@ -8,22 +14,25 @@ import { NewsDataType } from "@/types";
 import BreakingNews from "@/components/BreakingNews";
 import Categories from "@/components/Categories";
 import NewsList from "@/components/NewsList";
+import Loading from "@/components/Loading";
 
 type Props = {};
 
 const Page = (props: Props) => {
   const { top: safeTop } = useSafeAreaInsets();
   const [breakingNews, setBreakingNews] = useState<NewsDataType[]>([]);
+  const [news, setNews] = useState<NewsDataType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    // getBreakingNews();
+    getBreakingNews();
+    getNews();
   }, []);
 
   const getBreakingNews = async () => {
     try {
       setIsLoading(true);
-      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&country=in&language=en&image=1&removeduplicate=1&size=5`;
+      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&language=en&image=1&removeduplicate=1&size=5`;
 
       const response = await axios.get(URL);
       if (response && response.data) {
@@ -35,21 +44,43 @@ const Page = (props: Props) => {
     }
   };
 
+  const getNews = async (category: string = "") => {
+    try {
+      setIsLoading(true);
+      let categoryString = "";
+      if (category.length !== 0) {
+        categoryString = `&category=${category}`;
+      }
+      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&language=en&image=1&removeduplicate=1&size=10&${categoryString}`;
+
+      const response = await axios.get(URL);
+      if (response && response.data) {
+        setNews(response.data.results);
+        setIsLoading(false);
+      }
+    } catch (error: any) {
+      console.log("Error occured while fetching the breaking news.", error);
+    }
+  };
+
   const onCatChanged = (category: string) => {
     console.log("Category", category);
+    setNews([]);
+    getNews(category);
   };
 
   return (
-    <View style={(styles.container, { paddingTop: safeTop })}>
+    <ScrollView style={(styles.container, { paddingTop: safeTop })}>
       <Header />
       <SearchBar />
       {isLoading ? (
-        <ActivityIndicator size={"large"} />
+        <Loading size={"large"} />
       ) : (
         <BreakingNews newsList={breakingNews} />
       )}
       <Categories onCatChanged={onCatChanged} />
-    </View>
+      <NewsList newsList={news} />
+    </ScrollView>
   );
 };
 
--- components/NewsList.tsx
+++ components/NewsList.tsx
@@ -1,15 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 import { NewsDataType } from "@/types";
+import { Colors } from "@/constants/Colors";
+import Loading from "./Loading";
 
 type Props = {
-    newList: NewsDataType[]
-}
+  newsList: Array<NewsDataType>;
+};
 
-const NewsList = (props: Props) => {
+const NewsList = ({ newsList }: Props) => {
   return (
     <View style={styles.container}>
-      <Text>NewsList</Text>
+      {newsList.length === 0 ? (
+        <Loading size={"large"} />
+      ) : (
+        newsList.map((item, index) => (
+          <View key={index} style={styles.itemContainer}>
+            <Image source={{ uri: item.image_url }} style={styles.itemImg} />
+            <View style={styles.itemInfo}>
+              <Text style={styles.itemCategory}>{item.category}</Text>
+              <Text style={styles.itemTitle}>{item.title}</Text>
+              <View style={styles.itemSourceInfo}>
+                <Image
+                  source={{ uri: item.source_icon }}
+                  style={styles.itemSourceImage}
+                />
+                <Text style={styles.itemSourceName}>{item.source_name}</Text>
+              </View>
+            </View>
+          </View>
+        ))
+      )}
     </View>
   );
 };
@@ -19,5 +40,49 @@ export default NewsList;
 const styles = StyleSheet.create({
   container: {
     marginHorizontal: 20,
+    marginBottom: 50,
+  },
+  itemContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 20,
+    flex: 1,
+    gap: 10,
+  },
+  itemImg: {
+    width: 90,
+    height: 100,
+    borderRadius: 20,
+    marginRight: 10,
+  },
+  itemInfo: {
+    flex: 1,
+    gap: 10,
+    justifyContent: "space-between",
+  },
+  itemCategory: {
+    fontSize: 12,
+    color: Colors.darkGrey,
+    textTransform: "capitalize",
+  },
+  itemTitle: {
+    fontSize: 12,
+    fontWeight: "600",
+    color: Colors.black,
+  },
+  itemSourceInfo: {
+    flexDirection: "row",
+    gap: 8,
+    alignItems: "center",
+  },
+  itemSourceImage: {
+    width: 20,
+    height: 20,
+    borderRadius: 20,
+  },
+  itemSourceName: {
+    fontSize: 10,
+    fontWeight: "400",
+    color: Colors.darkGrey,
   },
 });
